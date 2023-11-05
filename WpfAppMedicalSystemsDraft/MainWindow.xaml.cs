a58965dff813ee919b0a488e5f37e5342712c008
misha <misha.yakushevich4@gmail.com>
2023-11-05 10:30:19 +0100

    Dodanie plików projektu

---
+++ WpfAppMedicalSystemsDraft/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppMedicalSystemsDraft
+{
+    class AppSettings
+    {
+        public string? ConnectionString{ get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var settings = ReadSettings();
+            if (settings?.ConnectionString == null)
+            {
+                MessageBox.Show("Error: Cannot load settings!");
+                Application.Current.Shutdown();
+            }
+            Console.WriteLine(settings?.ConnectionString);
+            InitializeComponent();
+            
+        }
+
+        private static AppSettings? ReadSettings()
+        {
+            string filePath = "data.bin";
+            string? connectionString;
+            if (File.Exists(filePath))
+            {
+ 
+                FileStream fs1 = new FileStream(filePath, FileMode.Open);
+                BinaryReader br = new BinaryReader(fs1);
+                try
+                {
+                    connectionString = br.ReadString();
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return null;
+                }
+                fs1.Close();
+            }
+            else
+            {
+                return null;
+            }
+            return new AppSettings{ConnectionString = connectionString};
+        }
+    }
+}
